7b4a99d34001b504020972414341a0f613b26e07
Vasu Singh <121501794+shivaay1392@users.noreply.github.com>
2022-12-29 08:14:30 +0530

    Add files via upload

---
+++ Gas sensor Interacing/Gas_sensor.ino
@@ -0,0 +1,24 @@
+#define gas 9
+#define LED 12
+
+void setup()
+{
+  pinMode(gas, INPUT);
+  pinMode(LED, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  float status1 = analogRead(gas);
+  if (status1<=100)
+  {
+    Serial.println("No smoke");
+    digitalWrite(LED, LOW);
+  }
+  else 
+  {
+    Serial.println("Smoke");
+    digitalWrite(LED, HIGH);
+  }
+}
